4e605696e3fccee1c9a62f71a0a0889a1e7ed4e2
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-08-03 21:56:08 -0300

    Initial commit

---
+++ src/main/java/com/giacomelli/RBAC_OAuth2/RbacOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.giacomelli.RBAC_OAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/giacomelli/RBAC_OAuth2/RbacOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacomelli.RBAC_OAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
